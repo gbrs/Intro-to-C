d3dafb5c99eb739ec9c215d5bf3cb7c3e8aefe47
gbrs <gbrs@mail.ru>
2021-07-17 14:15:12 +0300

    saw size different data types in Visual Studio and etc

--- C_first/C_first.cpp
+++ C_first/C_first.cpp
@@ -2,16 +2,27 @@
 //
 
 #include <iostream>
-double sin(double x);
+#include  <stdlib.h>
+#include  <time.h>
+
 
 int main()
 {
-	using namespace std;
-
-	double x = 3.1415 / 6;
-	cout << sin(x);
-	system("pause");
-
+	std::cout << "bool:\t\t" << sizeof(bool) << " bytes" << std::endl;
+	std::cout << "char:\t\t" << sizeof(char) << " bytes" << std::endl;
+	std::cout << "wchar_t:\t" << sizeof(wchar_t) << " bytes" << std::endl;
+	std::cout << "char16_t:\t" << sizeof(char16_t) << " bytes" << std::endl;
+	std::cout << "char32_t:\t" << sizeof(char32_t) << " bytes" << std::endl;
+	std::cout << "short:\t\t" << sizeof(short) << " bytes" << std::endl;
+	std::cout << "int:\t\t" << sizeof(int) << " bytes" << std::endl;
+	std::cout << "long:\t\t" << sizeof(long) << " bytes" << std::endl;
+	std::cout << "long long:\t" << sizeof(long long) << " bytes" << std::endl;
+	std::cout << "float:\t\t" << sizeof(float) << " bytes" << std::endl;
+	std::cout << "double:\t\t" << sizeof(double) << " bytes" << std::endl;
+	std::cout << "long double:\t" << sizeof(long double) << " bytes" << std::endl;
+	/*float a, b;
+	std::cin >> a >> b;
+	std::cout << a << b;*/
 	return 0;
 }
 
