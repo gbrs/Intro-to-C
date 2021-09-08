b57e6b47ac2555768ed97874ca67bc5d1f90012b
gbrs <gbrs@mail.ru>
2021-09-08 13:55:25 +0300

    finished lesson 5 exercises

--- C_first/C_first.cpp
+++ C_first/C_first.cpp
@@ -1,44 +1,44 @@
-﻿// 1.4.14
+﻿// 1.5.14
 
 #include <iostream>
 using namespace std;
 
-int main() {
-	int a1, b1, c1, a2, b2, c2;
-	cin >> a1 >> b1 >> c1;
-	cin >> a2 >> b2 >> c2;
+int main()
+{
+	int left, center, right, cnt = 0, mn = 10000;
+	bool flag = false;
 	
-	if (a1 > b1) swap(a1, b1);
-	if (b1 > c1) swap(b1, c1);
-	if (a1 > b1) swap(a1, b1);
-
-	if (a2 > b2) swap(a2, b2);
-	if (b2 > c2) swap(b2, c2);
-	if (a2 > b2) swap(a2, b2);
-
-	if (a1 == a2 && b1 == b2 && c1 == c2)
+	// ввод 2 первых элементов с проверкой не ноль ли
+	if ((cin >> left && left) && (cin >> center && center))
 	{
-		cout << "Boxes are equal";
-	}
-	else
-	{
-		if (a1 >= a2 && b1 >= b2 && c1 >= c2)
+		cin >> right;
+		while (right != 0)
 		{
-			cout << "The first box is larger than the second one";
-		}
-		else
-		{
-			if (a1 <= a2 && b1 <= b2 && c1 <= c2)
+			if (center > left && center > right)
 			{
-				cout << "The first box is smaller than the second one";
+				if (cnt + 1 < mn && flag)
+				{
+					mn = cnt + 1;
+				}
+				flag = true;  // флаг указывает на то, что один максимум уже был
+				cnt = 0;
 			}
 			else
 			{
-				cout << "Boxes are incomparable";
+				if (flag)
+				{
+					cnt++;
+				}	
 			}
+			left = center;
+			center = right;
+			cin >> right;
 		}
 	}
 
+	if (mn == 10000) mn = 0;
+	cout << mn;
+	
 	return 0;
 }
 
@@ -2289,6 +2289,82 @@ int main()
 	double x;
 	cin >> x;
 	cout << x - (float) (int) x;
+	return 0;
+}
+*/
+
+/*
+// 1.5.14
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int left, center, right, cnt = 0, mn = 10000;
+	bool flag = false;
+
+	// ввод 2 первых элементов с проверкой не ноль ли
+	if ((cin >> left && left) && (cin >> center && center))
+	{
+		cin >> right;
+		while (right != 0)
+		{
+			if (center > left && center > right)
+			{
+				if (cnt + 1 < mn && flag)
+				{
+					mn = cnt + 1;
+				}
+				flag = true;  // флаг указывает на то, что один максимум уже был
+				cnt = 0;
+			}
+			else
+			{
+				if (flag)
+				{
+					cnt++;
+				}
+			}
+			left = center;
+			center = right;
+			cin >> right;
+		}
+	}
+
+	if (mn == 10000) mn = 0;
+	cout << mn;
+
+	return 0;
+}
+*/
+
+/*
+// 1.5.13
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int now, left, center, right, cnt = 0;
+	if ((cin >> left && left) && (cin >> center && center))
+	{
+		cin >> right;
+		while (right != 0)
+		{
+			if (center > left && center > right)
+			{
+				cnt += 1;
+			}
+			left = center;
+			center = right;
+			cin >> right;
+		}
+	}
+
+	cout << cnt;
+
 	return 0;
 }
 */
